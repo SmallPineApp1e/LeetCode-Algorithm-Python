a6b551d818581acb792aeba364cfde28fb48dfcb
SmallPineApp1e <jie534838094@163.com>
2021-05-31 19:09:05 +0800

    【腾讯】反转链表

--- Tencent/突击链表/反转链表/solution.py
+++ Tencent/突击链表/反转链表/solution.py
@@ -8,3 +8,38 @@
 @Time: 2021/5/30 23:19
 @Desc:
 """
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+        pre, curr, next = None, head, head.next
+        while next is not None:
+            curr.next = pre
+            pre = curr
+            curr = next
+            next = curr.next
+        curr.next = pre
+        return curr
+
+
+if __name__ == '__main__':
+    s = Solution()
+    l5 = ListNode(5)
+    l4 = ListNode(4, l5)
+    l3 = ListNode(3, l4)
+    l2 = ListNode(2, l3)
+    l1 = ListNode(1, l2)
+    s.reverseList(l1)
