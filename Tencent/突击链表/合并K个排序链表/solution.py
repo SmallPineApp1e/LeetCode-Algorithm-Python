ecd5abff3fce1afd17e45ce9c962edef81e4b5cd
SmallPineApp1e <jie534838094@163.com>
2021-05-31 20:29:25 +0800

    【腾讯】合并K个链表

--- Tencent/突击链表/合并K个排序链表/solution.py
+++ Tencent/突击链表/合并K个排序链表/solution.py
@@ -8,3 +8,47 @@
 @Time: 2021/5/31 19:40
 @Desc:
 """
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        while len(lists) != 1:
+            l1, l2 = lists.pop(0), lists.pop(0)
+            newList = self.mergeTwoLists(l1, l2)
+            lists.append(newList)
+        return lists.pop(0)
+
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        newCurr, curr1, curr2 = dummy, l1, l2
+        while curr1 is not None and curr2 is not None:
+            if curr1.val <= curr2.val:
+                newCurr.next = curr1
+                curr1 = curr1.next
+            else:
+                newCurr.next = curr2
+                curr2 = curr2.next
+            print "%i " % newCurr.val,
+            newCurr = newCurr.next
+        if curr1 is not None:
+            newCurr.next = curr1
+        elif curr2 is not None:
+            newCurr.next = curr2
+        print "%i " % newCurr.val,
+        return dummy.next
--- Tencent/突击链表/合并K个排序链表/solution2.py
+++ Tencent/突击链表/合并K个排序链表/solution2.py
@@ -6,5 +6,34 @@
 @Software: PyCharm
 @File: solution2.py
 @Time: 2021/5/31 19:54
-@Desc:
+@Desc: 对K个链表的当前元素建立小根堆，使用O(logK)的时间选出最小元素，总时间复杂度优化成O(N*logK)
 """
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        import heapq
+        heap, dummy = [], ListNode(-1)
+        curr = dummy
+        for i in xrange(0, len(lists), 1):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i))
+                lists[i] = lists[i].next
+        while heap:
+            val, idx = heapq.heappop(heap)
+            curr.next = ListNode(val)
+            if lists[idx]:
+                heapq.heappush(heap, (lists[idx].val, idx))
+                lists[idx] = lists[idx].next
+            curr = curr.next
+        return dummy.next
