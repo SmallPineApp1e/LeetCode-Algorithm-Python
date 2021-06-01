930f4959b06ec15cd87f7220b73396ddbf92db2c
SmallPineApp1e <jie534838094@163.com>
2021-06-01 11:08:10 +0800

    【腾讯】删除链表中的节点

---
+++ Tencent/突击链表/删除链表中的节点/solution.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Author: SmallPineapple
+@Contact: jie534838094@163.com
+@Software: PyCharm
+@File: solution.py
+@Time: 2021/6/1 10:50
+@Desc:
+"""
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
