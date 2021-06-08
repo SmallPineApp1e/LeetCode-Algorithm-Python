a1b45f4bdc27428d0117e0e36c8713314eb59dd5
SmallPineApp1e <jie534838094@163.com>
2021-06-08 10:58:30 +0800

    [腾讯] 二叉树中的最大路径和

--- Tencent/排序与搜索/二叉树中的最大路径和/solution.py
+++ Tencent/排序与搜索/二叉树中的最大路径和/solution.py
@@ -8,3 +8,33 @@
 @Time: 2021/6/3 20:57
 @Desc:
 """
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def __init__(self):
+        self.res = 0
+
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.dfs(root)
+        return self.res
+
+    def dfs(self, root):
+        if not root:
+            return 0
+        leftSum = self.dfs(root.left)
+        rightSum = self.dfs(root.right)
+        self.res = max(leftSum + rightSum + root.val, self.res)
+        outputMaxSum = max(leftSum, rightSum) + root.val
+        return outputMaxSum if outputMaxSum > 0 else 0
