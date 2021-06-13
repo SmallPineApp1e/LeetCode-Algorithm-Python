c5b29e3fe5500d9feb965b3067b694cffacb9399
SmallPineApp1e <jie534838094@163.com>
2021-06-13 17:29:17 +0800

    [腾讯] 格雷编码

--- Tencent/回溯算法/格雷编码/solution.py
+++ Tencent/回溯算法/格雷编码/solution.py
@@ -6,7 +6,7 @@
 @Software: PyCharm
 @File: solution.py
 @Time: 2021/6/12 23:37
-@Desc:
+@Desc: 精选题解：https://leetcode-cn.com/problems/gray-code/solution/gray-code-jing-xiang-fan-she-fa-by-jyd/
 """
 
 
@@ -16,3 +16,14 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
+        res, head = [0], 1
+        for i in xrange(0, n):
+            for j in xrange(len(res) - 1, -1, -1):
+                res.append(res[j] + head)
+            head <<= 1
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.grayCode(3)
